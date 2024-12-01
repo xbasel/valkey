c271b1b305dac7fa767e931623dc638ba4928612
xbasel <103044017+xbasel@users.noreply.github.com>
2024-12-01 23:29:42 +0200

    Fix test_reclaimFilePageCache to avoid tmpfs Skip test when tmp is tmpfs as fadvise(FADV_DONTNEED) has no effect on memory-backed filesystems.
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/unit/test_util.c
+++ src/unit/test_util.c
@@ -1,6 +1,8 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/statfs.h>
+#include <linux/magic.h>
 
 #include "../config.h"
 #include "../util.h"
@@ -283,6 +285,22 @@ static int cache_exist(int fd) {
 }
 #endif
 
+/**
+ * Check if /tmp/ is disk-based
+ * This is required because we are testing fadvise(FADV_DONTNEED), which operates on disk-backed files.
+ */
+int is_tmp_disk_based(void) {
+    struct statfs buf;
+
+    /* Check if /tmp is memory-backed (e.g., tmpfs) */
+    if (statfs("/tmp", &buf) == 0) {
+        if (buf.f_type != TMPFS_MAGIC) { // Not tmpfs, use /tmp
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int test_reclaimFilePageCache(int argc, char **argv, int flags) {
     UNUSED(argc);
     UNUSED(argv);
@@ -291,7 +309,12 @@ int test_reclaimFilePageCache(int argc, char **argv, int flags) {
     if (flags & UNIT_TEST_VALGRIND) return 0;
 
 #if defined(__linux__)
+    if (!is_tmp_disk_based()) {
+        return 0; // Can not be tested on a ram drive.
+    }
+    
     char *tmpfile = "/tmp/redis-reclaim-cache-test";
+
     int fd = open(tmpfile, O_RDWR | O_CREAT, 0644);
     TEST_ASSERT(fd >= 0);
 
