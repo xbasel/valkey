e8078b7315250dc052b4020a4ea73471a8c0e4a9
Guillaume Koenig <106696198+knggk@users.noreply.github.com>
2024-12-08 07:30:07 -0500

    Allow MEMORY MALLOC-STATS and MEMORY PURGE during loading phase (#1317)
    - Enable investigation of memory issues during loading
    - Previously, all memory commands were rejected with LOADING error
    (except memory help)
    - `MEMORY MALLOC-STATS` and `MEMORTY PURGE` are now allowed
    as they don't depend on the dataset
    - `MEMORY STATS` and `MEMORY USAGE KEY` remain disallowed
    
    Fixes #1299
    
    Signed-off-by: Guillaume Koenig <knggk@amazon.com>
    Signed-off-by: Binbin <binloveplay1314@qq.com>
    Co-authored-by: Binbin <binloveplay1314@qq.com>

--- tests/unit/introspection.tcl
+++ tests/unit/introspection.tcl
@@ -1042,6 +1042,49 @@ test {config during loading} {
     }
 } {} {external:skip}
 
+test {MEMORY commands during loading} {
+    start_server [list overrides [list key-load-delay 50 loading-process-events-interval-bytes 1024]] {
+        # Set up some initial data
+        r debug populate 100000 key 1000
+
+        # Save and restart
+        r save
+        restart_server 0 false false
+
+        # At this point, keys are loaded one at time, busy looping 50usec
+        # between each. Further, other events are processed every 1024 bytes
+        # of RDB. We're sending all our commands deferred, so they have a
+        # chance to be processed all at once between loading two keys.
+
+        set rd [valkey_deferring_client]
+
+        # Allowed during loading
+        $rd memory help
+        $rd memory malloc-stats
+        $rd memory purge
+
+        # Disallowed during loading (because directly dependent on the dataset)
+        $rd memory doctor
+        $rd memory stats
+        $rd memory usage key:1
+
+        # memory help
+        assert_match {{MEMORY <subcommand> *}} [$rd read]
+        # memory malloc-stats
+        assert_match {*alloc*} [$rd read]
+        # memory purge
+        assert_match OK [$rd read]
+        # memory doctor
+        assert_error {*LOADING*} {$rd read}
+        # memory stats
+        assert_error {*LOADING*} {$rd read}
+        # memory usage key:1
+        assert_error {*LOADING*} {$rd read}
+
+        $rd close
+    }
+} {} {external:skip}
+
 test {CONFIG REWRITE handles rename-command properly} {
     start_server {tags {"introspection"} overrides {rename-command {flushdb badger}}} {
         assert_error {ERR unknown command*} {r flushdb}
