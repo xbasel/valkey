211b250aad1db6e7a9480fd3c896786d25210fb2
Binbin <binloveplay1314@qq.com>
2025-01-11 10:43:18 +0800

    Do election in order based on failed primary rank to avoid voting conflicts (#1018)
    When multiple primary nodes fail simultaneously, the cluster can not recover
    within the default effective time (data_age limit). The main reason is that
    the vote is without ranking among multiple replica nodes, which case too many
    epoch conflicts.
    
    Therefore, we introduced into ranking based on the failed primary shard-id.
    Introduced a new failed_primary_rank var, this var means the rank of this
    myself instance in the context of all failed primary list. This var will be
    used in failover and we will do the failover election packets in order based
    on the rank, this can effectively avoid the voting conflicts.
    
    If a single primary is down, the behavior is the same as before. If multiple
    primaries are down, their replica election initiation time will be delayed
    by 500ms according to the ranking.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- tests/unit/cluster/failover2.tcl
+++ tests/unit/cluster/failover2.tcl
@@ -62,9 +62,7 @@ start_cluster 3 4 {tags {external:skip cluster} overrides {cluster-ping-interval
         verify_no_log_message -3 "*Failover attempt expired*" 0
         verify_no_log_message -6 "*Failover attempt expired*" 0
     }
-
 } ;# start_cluster
-
 
 start_cluster 7 3 {tags {external:skip cluster} overrides {cluster-ping-interval 1000 cluster-node-timeout 5000}} {
     test "Primaries will not time out then they are elected in the same epoch" {
@@ -102,3 +100,36 @@ start_cluster 7 3 {tags {external:skip cluster} overrides {cluster-ping-interval
         resume_process [srv -2 pid]
     }
 } ;# start_cluster
+
+run_solo {cluster} {
+    start_cluster 32 15 {tags {external:skip cluster} overrides {cluster-ping-interval 1000 cluster-node-timeout 15000}} {
+        test "Multiple primary nodes are down, rank them based on the failed primary" {
+            # Killing these primary nodes.
+            for {set j 0} {$j < 15} {incr j} {
+                pause_process [srv -$j pid]
+            }
+
+            # Make sure that a node starts failover.
+            wait_for_condition 1000 100 {
+                [s -40 role] == "master"
+            } else {
+                fail "No failover detected"
+            }
+
+            # Wait for the cluster state to become ok.
+            for {set j 0} {$j < [llength $::servers]} {incr j} {
+                if {[process_is_paused [srv -$j pid]]} continue
+                wait_for_condition 1000 100 {
+                    [CI $j cluster_state] eq "ok"
+                } else {
+                    fail "Cluster node $j cluster_state:[CI $j cluster_state]"
+                }
+            }
+
+            # Resuming these primary nodes, speed up the shutdown.
+            for {set j 0} {$j < 15} {incr j} {
+                resume_process [srv -$j pid]
+            }
+        }
+    } ;# start_cluster
+} ;# run_solo
