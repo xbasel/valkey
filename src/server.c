3a1d65b064d577365513821ebcd521dca9a8ddac
xbasel <103044017+xbasel@users.noreply.github.com>
2025-07-28 23:51:41 +0300

    Update src/server.c
    Co-authored-by: Ran Shidlansik <ranshid@amazon.com>

--- src/server.c
+++ src/server.c
@@ -6068,7 +6068,7 @@ sds genValkeyInfoString(dict *section_dict, int all_sections, int everything) {
                 "sync_partial_ok:%lld\r\n", server.stat_sync_partial_ok,
                 "sync_partial_err:%lld\r\n", server.stat_sync_partial_err,
                 "expired_keys:%lld\r\n", server.stat_expiredkeys,
-                "expired_subkeys:%lld\r\n", server.stat_expiredfields,
+                "expired_fields:%lld\r\n", server.stat_expiredfields,
                 "expired_stale_perc:%.2f\r\n", server.stat_expired_stale_perc * 100,
                 "expired_time_cap_reached_count:%lld\r\n", server.stat_expired_time_cap_reached_count,
                 "expire_cycle_cpu_milliseconds:%lld\r\n", server.stat_expire_cycle_time_used / 1000,
